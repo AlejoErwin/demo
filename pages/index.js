94e399d5d652c018c1702e5fd4f86fff27b45804
AlejoErwin <alejo.gutierrez.erwin@gmail.com>
2022-03-28 16:45:33 -0400

    shopify demo

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import { Heading, Page } from "@shopify/polaris";
+
+function Index() {
+  return (
+    //<Page>
+    //   <Heading>
+    //    Shopify app with Node and React{" "}
+    //    <span role="img" aria-label="tada emoji">
+    //    🎉🎉🎉🎉
+    //      Nuevos
+    //    </span>
+    //  </Heading>
+    //</Page>
+    <div className="inicioNav" >
+        Nuevo inicio<br/>
+        
+        Pagos
+    </div>
+  );
+}
+export default Index;
